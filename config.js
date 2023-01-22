68b383f4f79d6cb810cf836c3fba8fa7c0b151c0
tienphan120307 <101572989+213214432rw@users.noreply.github.com>
2023-01-22 19:48:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Không danh, không lợi đời không nể',
     messDesc: 'Không tiền, không bạc gái không theo',
     btnAccept: 'Bấm vô đây để gửi lời chúc đến bạn bè',
-    messLink: 'https://www.facebook.com/messenger_media/?attachment_id=1332367570938133&message_id=mid.%24cAAAB-qPwNDmMCrGxg2F2Wx71gebv&thread_id=100041927202563' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://scontent.fsgn13-4.fna.fbcdn.net/v/t1.15752-9/325933771_728011638738883_3147273842462000645_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=ae9488&_nc_ohc=bAHzYx_PchcAX83cBPI&_nc_ht=scontent.fsgn13-4.fna&oh=03_AdRixCCHY_mrkNAptzntVGr5GCwel4FzoIs5BNi4EEjKZQ&oe=63F4A98A' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
